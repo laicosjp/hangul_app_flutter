8156f544f76eebecd90eb48b18840d61e7ea60e6
hulk-higakijin <hm385.chejptks@gmail.com>
2024-04-14 17:34:03 +0900

    style: courseDescriptionを表示

--- lib/resources/pages/course_page.dart
+++ lib/resources/pages/course_page.dart
@@ -9,11 +9,10 @@ class CoursePage extends NyStatefulWidget {
 
 class _CoursePageState extends NyState<CoursePage> {
   final String courseTitle = 'ハングル検定5級';
+  final String courseDescription = '韓国語の入門者向け！ハングル検定5級の単語を学ぼう！';
 
   @override
-  init() async {
-    
-  }
+  init() async {}
 
   /// Use boot if you need to load data before the [view] is rendered.
   // @override
@@ -26,7 +25,11 @@ class _CoursePageState extends NyState<CoursePage> {
     return Scaffold(
       appBar: AppBar(title: Text(courseTitle)),
       body: SafeArea(
-        child: Container(),
+        child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [Text(courseDescription)],
+            )),
       ),
     );
   }
