fb65ae99e7843e5c32426431d1c41779453107b9
hulk-higakijin <hm385.chejptks@gmail.com>
2024-05-02 17:45:46 +0900

    style: course_pageのデザインを変える

--- lib/resources/pages/course_page.dart
+++ lib/resources/pages/course_page.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_app/app/models/lesson.dart';
-import 'package:flutter_app/bootstrap/helpers.dart';
 import 'package:flutter_app/resources/services/lessons_service.dart';
+import 'package:flutter_app/resources/widgets/safearea_widget.dart';
+import 'package:gap/gap.dart';
 import 'package:nylo_framework/nylo_framework.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class CoursePage extends NyStatefulWidget {
   static const path = '/course';
@@ -20,39 +24,211 @@ class _CoursePageState extends NyState<CoursePage> {
     _lessons = await _lessonsService.findAll(courseId: int.parse(queryParameters()['course_id']));
   }
 
+  // @override
+  // Widget view(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: Text("単語一覧"), automaticallyImplyLeading: false),
+  //     body: SafeArea(
+  //       child: Container(
+  //           padding: EdgeInsets.all(20),
+  //           child: Column(
+  //             children: [
+  //               Expanded(
+  //                 child: GridView.builder(
+  //                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+  //                     crossAxisCount: 2, // 2つのアイテムを横に並べる
+  //                     crossAxisSpacing: 10, // 横のスペース
+  //                     mainAxisSpacing: 10, // 縦のスペース
+  //                     childAspectRatio: 4.0,
+  //                   ),
+  //                   itemCount: _lessons.length,
+  //                   itemBuilder: (context, index) {
+  //                     return OutlinedButton(
+  //                         onPressed: () {
+  //                           routeTo('/word', queryParameters: {"lessonId": _lessons[index].id.toString()});
+  //                         },
+  //                         child: Text(_lessons[index].title),
+  //                         style: OutlinedButton.styleFrom(
+  //                           side: BorderSide(color: ThemeColor.get(context).primaryAccent),
+  //                           foregroundColor: ThemeColor.get(context).buttonPrimaryContent,
+  //                         ));
+  //                   },
+  //                 ),
+  //               ),
+  //             ],
+  //           )),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget view(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("単語一覧"), automaticallyImplyLeading: false),
-      body: SafeArea(
+      appBar: AppBar(title: Text("ハングル検定5級")),
+      body: SafeAreaWidget(
         child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, // 2つのアイテムを横に並べる
-                      crossAxisSpacing: 10, // 横のスペース
-                      mainAxisSpacing: 10, // 縦のスペース
-                      childAspectRatio: 4.0,
+          child: Column(
+            children: [
+              Container(
+                height: 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.8,
+                      center: Text("1", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
                     ),
-                    itemCount: _lessons.length,
-                    itemBuilder: (context, index) {
-                      return OutlinedButton(
-                          onPressed: () {
-                            routeTo('/word', queryParameters: {"lessonId": _lessons[index].id.toString()});
-                          },
-                          child: Text(_lessons[index].title),
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: ThemeColor.get(context).primaryAccent),
-                            foregroundColor: ThemeColor.get(context).buttonPrimaryContent,
-                          ));
-                    },
-                  ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.3,
+                      center: Text("2", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.0,
+                      center: Text("3", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.0,
+                      center: Text("4", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.0,
+                      center: Text("5", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.0,
+                      center: Text("6", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                    Gap(16),
+                    CircularPercentIndicator(
+                      radius: 26.0,
+                      lineWidth: 7,
+                      percent: 0.0,
+                      center: Text("7", style: TextStyle(fontWeight: FontWeight.w600)),
+                      backgroundColor: Colors.grey,
+                      progressColor: Colors.lightGreen,
+                    ),
+                  ],
                 ),
-              ],
-            )),
+              ),
+              Gap(40),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text("ハングル検定5級 Lesson1", style: TextStyle(fontWeight: FontWeight.w600)),
+              ),
+              Gap(20),
+              Container(
+                  height: 200,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CircularPercentIndicator(
+                          radius: 80.0,
+                          lineWidth: 14,
+                          percent: 0.8,
+                          center: Center(
+                            child: Text(
+                              "80%",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          backgroundColor: Colors.grey,
+                          progressColor: Colors.lightGreen,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                  child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.circle,
+                                    color: Colors.lightGreen,
+                                    size: 18,
+                                  ),
+                                  Text("覚えた", style: TextStyle(fontSize: 14)),
+                                  Spacer(),
+                                  Text("80", style: TextStyle(fontSize: 34)),
+                                  Gap(4),
+                                  Text("%")
+                                ],
+                              )),
+                              Expanded(
+                                  child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.circle,
+                                    color: Colors.grey,
+                                    size: 18,
+                                  ),
+                                  Text("未修得", style: TextStyle(fontSize: 14)),
+                                  Spacer(),
+                                  Text("20", style: TextStyle(fontSize: 24)),
+                                  Gap(4),
+                                  Text("%")
+                                ],
+                              )),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+              Gap(20),
+              Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("苦手"),
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.grey, foregroundColor: Colors.white),
+                      ),
+                    ),
+                    Gap(10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("未学習"),
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.orange, foregroundColor: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
