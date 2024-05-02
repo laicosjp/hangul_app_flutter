c84072fc74a2bf2a5da42c774f480cbdb6ec6d79
hulk-higakijin <hm385.chejptks@gmail.com>
2024-05-02 13:38:04 +0900

    style: コース一覧でダッシュボードテンプレを使用

--- lib/resources/pages/courses_page.dart
+++ lib/resources/pages/courses_page.dart
@@ -30,15 +30,69 @@ class _CoursesPageState extends NyState<CoursesPage> {
       appBar: AppBar(title: Text("コース"), automaticallyImplyLeading: false),
       body: SafeArea(
         child: ListView.builder(
-          itemCount: courses.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(courses[index].title),
-              onTap: () {
-                routeTo('/course', queryParameters: {'course_id': courses[index].id.toString()});
-              },
+          physics: NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemBuilder: (context, position) {
+            return Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image(
+                        image: NetworkImage("https://pbs.twimg.com/media/GMU4OtFW0AEa3Rk?format=jpg&name=large"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Container(
+                      // width: width - (width / 3) - 16,
+                      height: 100,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(courses[position].title, style: TextStyle(fontWeight: FontWeight.w600)),
+                          SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Icon(Icons.timelapse, size: 16, color: Colors.orange),
+                                  SizedBox(width: 2),
+                                  Text(
+                                    "04 / 100",
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                              GestureDetector(
+                                child: Row(
+                                  children: <Widget>[
+                                    Text("Try", style: TextStyle(fontSize: 14)),
+                                    Icon(Icons.keyboard_arrow_right, size: 16, color: Colors.orange),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             );
           },
+          itemCount: courses.length,
         ),
       ),
     );
