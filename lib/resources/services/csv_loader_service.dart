5ea0cc269a67407b08f43ec18dd0c81f26722d51
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-28 17:55:46 +0900

    feat: delete files related csvToJson

--- lib/resources/services/csv_loader_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:csv/csv.dart';
-
-class CsvLoaderService {
-  Future<List<List<String>>> convertCsv({required String model}) async {
-    final _rawCSV = await rootBundle.loadString("public/assets/csv/$model.csv");
-    return CsvToListConverter().convert(_rawCSV).map((list) => list.map((item) => item.toString()).toList()).toList();
-  }
-}
