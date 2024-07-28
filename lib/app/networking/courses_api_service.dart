a56babbb584b2d31ac592d60f5d92c79c1da40f9
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-28 11:35:45 +0900

    feat: get courses from api

--- lib/app/networking/courses_api_service.dart
+++ lib/app/networking/courses_api_service.dart
@@ -3,9 +3,9 @@ import 'package:flutter_app/app/models/course.dart';
 import '/config/decoders.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
-
 class CoursesApiService extends NyApiService {
-  CoursesApiService({BuildContext? buildContext}) : super(buildContext, decoders: modelDecoders);
+  CoursesApiService({BuildContext? buildContext})
+      : super(buildContext, decoders: modelDecoders);
 
   @override
   String get baseUrl => getEnv('API_BASE_URL');
@@ -13,6 +13,9 @@ class CoursesApiService extends NyApiService {
   Future<List<Course>?> findAll() async {
     return await network<List<Course>>(
       request: (request) => request.get("/courses"),
+      handleFailure: (DioException) {
+        return [];
+      },
     );
   }
 }
