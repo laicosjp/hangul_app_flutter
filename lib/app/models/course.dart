bbb307c5731041c656f99317bc30c969772a2389
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-28 17:58:55 +0900

    feat: add words in course model

--- lib/app/models/course.dart
+++ lib/app/models/course.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_app/app/models/word.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
 /// Course Model.
@@ -9,12 +10,15 @@ class Course extends Model {
   final String thumbnailUrl;
   final int wordsCount;
 
+  List<Word>? words;
+
   Course({
     required this.id,
     required this.title,
     required this.slug,
     required this.thumbnailUrl,
     required this.wordsCount,
+    this.words,
   });
 
   factory Course.fromJson(data) {
@@ -24,6 +28,9 @@ class Course extends Model {
       slug: data['slug'],
       thumbnailUrl: data['thumbnailUrl'],
       wordsCount: data['wordsCount'],
+      words: data['words'] != null
+          ? List<Word>.from(data['words'].map((word) => Word.fromJson(word)))
+          : [],
     );
   }
 
