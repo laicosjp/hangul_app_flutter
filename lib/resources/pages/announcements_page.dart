160bf1f546c5587922b1a39ddd5f9bca4e340b25
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-08 15:48:19 +0900

    style: add tab in appbar

--- lib/resources/pages/announcements_page.dart
+++ lib/resources/pages/announcements_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/resources/widgets/general_announcements_widget.dart';
-import 'package:flutter_app/resources/widgets/safearea_widget.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
 class AnnouncementsPage extends NyStatefulWidget {
@@ -21,11 +20,33 @@ class _AnnouncementsPageState extends NyState<AnnouncementsPage> {
   // }
 
   @override
-  Widget view(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Notifications")),
-      body: SafeAreaWidget(
-        child: GeneralAnnouncements(),
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: const Text('Notifications',
+              style: TextStyle(color: Colors.white)),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(child: Text("For you")),
+              Tab(child: Text("News")),
+            ],
+            dividerColor: Colors.orange,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            indicatorColor: Colors.white,
+          ),
+          backgroundColor: Colors.orange,
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            GeneralAnnouncements(),
+            GeneralAnnouncements(),
+          ],
+        ),
       ),
     );
   }
