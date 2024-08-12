a27b875f6b16399ec1e5f1ba9f61437d10a29f9e
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-12 17:30:29 +0900

    feat: result page

---
+++ lib/resources/pages/result_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class ResultPage extends NyStatefulWidget {
+  static const path = '/result';
+  
+  ResultPage({super.key}) : super(path, child: _ResultPageState());
+}
+
+class _ResultPageState extends NyState<ResultPage> {
+
+  @override
+  init() async {
+
+  }
+  
+  /// Use boot if you need to load data before the [view] is rendered.
+  // @override
+  // boot() async {
+  //
+  // }
+
+  @override
+  Widget view(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Result")
+      ),
+      body: SafeArea(
+         child: Container(),
+      ),
+    );
+  }
+}
