1bccea1d44c5433a4bea5d4c24b68e84ea3457d7
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-21 20:34:57 +0900

    feat: レコード数によって表示するボタンの種類を分ける

--- lib/resources/pages/course_page.dart
+++ lib/resources/pages/course_page.dart
@@ -27,156 +27,162 @@ class _CoursePageState extends NyState<CoursePage> {
   @override
   Widget view(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(_course?.title ?? '')),
-        body: SafeAreaWidget(
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  child: Container(
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                _course?.title ?? '',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Spacer(),
-                            SizedBox(
-                              width: 40,
-                              height: 40,
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  elevation: 0,
-                                  padding: EdgeInsets.zero,
-                                  backgroundColor:
-                                      Color(0xff465AFF).withOpacity(0.1),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12.0),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.share_outlined,
-                                    color: Color(0xff465AFF),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Gap(8),
-                            SizedBox(
-                              width: 40,
-                              height: 40,
-                              child: ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  elevation: 0,
-                                  padding: EdgeInsets.zero,
-                                  backgroundColor: Colors.green.shade50,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12.0),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.public_outlined,
-                                    color: Colors.orange,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(children: [
-                          Icon(
-                            Icons.menu_book,
-                            color: Colors.black54,
-                            size: 16,
-                          ),
-                          Gap(4),
-                          Text(
-                            "${_course?.wordsCount} words",
-                            style:
-                                TextStyle(color: Colors.black54, fontSize: 14),
-                          ),
-                        ])
-                      ],
-                    ),
-                  ),
-                ),
-                Gap(8),
-                Divider(),
-                Gap(8),
-                Container(
-                  alignment: Alignment.centerLeft,
+      appBar: AppBar(title: Text(_course?.title ?? '')),
+      body: SafeAreaWidget(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                child: Container(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Words you'll learn",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Gap(8),
-                      Wrap(
-                        spacing: 2,
-                        runSpacing: 2,
-                        children:
-                            (_course?.exampleWords ?? []).take(15).map((word) {
-                          return Chip(
-                            label: Text(word.name),
-                            backgroundColor: Color(0xffEBEBEb),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                              side: BorderSide(
-                                color: Colors.transparent,
-                                width: 0,
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              _course?.title ?? '',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          );
-                        }).toList(),
-                      ),
-                      Gap(20),
-                      Text(
-                        "Progress",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Gap(8),
-                      HorizontalBar(
-                        data: [
-                          BarSegment(
-                              percentage: (_course?.correctedWordsCount ?? 0)
-                                  .toDouble(),
-                              color: Colors.lightGreen),
-                          BarSegment(
-                              percentage: (_course?.incorrectedWordsCount ?? 0)
-                                  .toDouble(),
-                              color: Colors.lightBlue),
-                          BarSegment(
-                              percentage: (_course?.unstudiedWordsCount ?? 0)
-                                  .toDouble(),
-                              color: Colors.black12),
+                          ),
+                          Spacer(),
+                          SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                padding: EdgeInsets.zero,
+                                backgroundColor:
+                                    Color(0xff465AFF).withOpacity(0.1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.share_outlined,
+                                  color: Color(0xff465AFF),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Gap(8),
+                          SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                padding: EdgeInsets.zero,
+                                backgroundColor: Colors.green.shade50,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  Icons.public_outlined,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
+                      Row(children: [
+                        Icon(
+                          Icons.menu_book,
+                          color: Colors.black54,
+                          size: 16,
+                        ),
+                        Gap(4),
+                        Text(
+                          "${_course?.wordsCount} words",
+                          style: TextStyle(color: Colors.black54, fontSize: 14),
+                        ),
+                      ])
                     ],
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              Gap(8),
+              Divider(),
+              Gap(8),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Words you'll learn",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gap(8),
+                    Wrap(
+                      spacing: 2,
+                      runSpacing: 2,
+                      children:
+                          (_course?.exampleWords ?? []).take(15).map((word) {
+                        return Chip(
+                          label: Text(word.name),
+                          backgroundColor: Color(0xffEBEBEb),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            side: BorderSide(
+                              color: Colors.transparent,
+                              width: 0,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                    Gap(20),
+                    Text(
+                      "Progress",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gap(8),
+                    HorizontalBar(
+                      data: [
+                        BarSegment(
+                            percentage:
+                                (_course?.correctedWordsCount ?? 0).toDouble(),
+                            color: Colors.lightGreen),
+                        BarSegment(
+                            percentage: (_course?.incorrectedWordsCount ?? 0)
+                                .toDouble(),
+                            color: Colors.lightBlue),
+                        BarSegment(
+                            percentage:
+                                (_course?.unstudiedWordsCount ?? 0).toDouble(),
+                            color: Colors.black12),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-        bottomNavigationBar: StartQuizButton(courseId: _course?.id ?? 0));
+      ),
+      bottomNavigationBar: StartQuizButton(
+        courseId: _course?.id ?? 0,
+        wordsCount: _course?.wordsCount ?? 0,
+        correctWordsCount: _course?.correctedWordsCount ?? 0,
+        incorrectWordsCount: _course?.incorrectedWordsCount ?? 0,
+        unstudiedWordsCount: _course?.unstudiedWordsCount ?? 0,
+      ),
+    );
   }
 }
--- lib/resources/widgets/start_quiz_button_widget.dart
+++ lib/resources/widgets/start_quiz_button_widget.dart
@@ -3,7 +3,49 @@ import 'package:nylo_framework/nylo_framework.dart';
 
 class StartQuizButton extends StatelessWidget {
   final int courseId;
-  const StartQuizButton({super.key, required this.courseId});
+  final int wordsCount;
+  final int correctWordsCount;
+  final int incorrectWordsCount;
+  final int unstudiedWordsCount;
+
+  const StartQuizButton({
+    super.key,
+    required this.courseId,
+    required this.wordsCount,
+    required this.correctWordsCount,
+    required this.incorrectWordsCount,
+    required this.unstudiedWordsCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // 着手前：「learn this course」
+    if (wordsCount == unstudiedWordsCount) {
+      return LearnThisCourse(courseId: courseId);
+    }
+
+    // 未学習が0：「retry incorrect words」
+    if (unstudiedWordsCount == 0) {
+      return RetryIncorrectWords(courseId: courseId);
+    }
+
+    // すべてcorrect: 「restart this course」
+    if (wordsCount == correctWordsCount) {
+      return RestartThisCourse(courseId: courseId);
+    }
+
+    // 着手中：「learn this course」「retry incorrect words」
+    return LearntThisCourseAndRetryIncorrectWords(courseId: courseId);
+  }
+}
+
+class LearnThisCourse extends StatelessWidget {
+  const LearnThisCourse({
+    super.key,
+    required this.courseId,
+  });
+
+  final int courseId;
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +69,117 @@ class StartQuizButton extends StatelessWidget {
         ),
         child: Text(
           'Learn this course',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class RetryIncorrectWords extends StatelessWidget {
+  const RetryIncorrectWords({
+    super.key,
+    required this.courseId,
+  });
+
+  final int courseId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          routeTo(
+            '/quiz',
+            queryParameters: {
+              'courseId': courseId.toString(),
+            },
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: Text(
+          'Retry incorrect words',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class RestartThisCourse extends StatelessWidget {
+  const RestartThisCourse({
+    super.key,
+    required this.courseId,
+  });
+
+  final int courseId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          routeTo(
+            '/quiz',
+            queryParameters: {
+              'courseId': courseId.toString(),
+            },
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Colors.grey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: Text(
+          'Ret records and start to learn again',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class LearntThisCourseAndRetryIncorrectWords extends StatelessWidget {
+  const LearntThisCourseAndRetryIncorrectWords({
+    super.key,
+    required this.courseId,
+  });
+
+  final int courseId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          routeTo(
+            '/quiz',
+            queryParameters: {
+              'courseId': courseId.toString(),
+            },
+          );
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: Text(
+          'two buttons',
           style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
